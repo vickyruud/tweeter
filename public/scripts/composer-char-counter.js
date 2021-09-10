5871c08a71af7cee2d0703a660acd170846fdd7f
vickyruud <aprajit.k@gmail.com>
2021-09-10 09:38:27 -0400

    add character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  const textArea = document.querySelector('main textarea');
+  const wordCount = document.getElementById('word-count')
+  textArea.addEventListener("keyup", function(event)  {
+    let characters = textArea.value.split('');
+    wordCount.innerText = 140 - characters.length;
+
+    if (wordCount.innerText < 0) {
+      wordCount.style.color = 'red';
+    } else {
+      wordCount.style.color = '';
+    }
+    
+  })
+
+});
